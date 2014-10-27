6d0ce3dbf9b0c7705f07c2c08bf116d7032c4c2e
Diogo Brito <brito@grupodpg.com.br>
2014-10-27 18:29:25 -0200

    404

--- layout/themes/pages/home/home.php
+++ layout/themes/pages/home/home.php
@@ -1,10 +1,20 @@
 <?php
+    $page_control = false;
     /*
      * Carregamento de conteúdo para a página inicial e home
      */
-    RequirePage('index|home', 'pages/home/welcome_message.php');
+    if(RequirePage('index|home', 'pages/home/welcome_message.php'))
+    {
+    	$page_control = true;
+    }
 
     /*
      * Carregamento de conteúdo para a página component
      */
-    RequirePage('home/component', 'pages/home/component_message.php');
+    elseif(RequirePage('home/component', 'pages/home/component_message.php'))
+    {
+    	$page_control = true;
+    }
+
+    if($page_control === false)
+    	header('Location: ' . BaseURL() . '404');
