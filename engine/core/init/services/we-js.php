02dd3d50ca1fcfe2e891b9b670b3d1e820300eab
Diogo Brito <brito@grupodpg.com.br>
2014-12-03 21:33:21 -0200

    Helper JavaScript basic functions

---
+++ engine/core/init/services/we-js.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * We-JS
+ * Este serviço tem como objetivo criar algumas funções para javascript
+ *
+ * @author Diogo Brito <diogo@weverest.com.br>
+ */
+
+//Pasta que irá ser gerado o arquivo
+$jsPath = WE_THEME_PATH . 'assets' . DS . 'js' . DS . 'scripts' . DS . 'weframework' . DS;
+
+if(is_writable(WE_THEME_PATH . 'assets' . DS . 'js'))
+{
+    //Nome do arquivo
+    $filename = 'weframewok.js';
+    //Verifica se o arquivo existe
+    if(!file_exists($jsPath . $filename))
+    {
+        //Verifica se o diretório existe, caso contrário cria-se
+        if(!is_dir($jsPath))
+            mkdir($jsPath, 0755, true);
+
+        //O dire´torio existe?
+        if(is_dir($jsPath))
+        {
+            //Nome do Arquivo
+            $jsFile = $jsPath . $filename;
+            //Criação do arquivo
+            touch($jsFile);
+
+            if(is_file($jsFile))
+            {
+                //Conteúdo do arquivo
+                //Comentários - START
+                $jsContent = "/*" . PHP_EOL;
+                $jsContent .= "# WeFramework helper base functions". PHP_EOL;
+                $jsContent .= "# File update at " . date('Y-m-d H:i:s') . PHP_EOL;
+                $jsContent .= "*/" . PHP_EOL . PHP_EOL . PHP_EOL;
+                //Comentários - END
+
+                //Função BaseUrl()- START
+                $jsContent .= "function BaseUrl()". PHP_EOL;
+                $jsContent .= "{". PHP_EOL;
+                $jsContent .= "\t";
+                $jsContent .= "return \"" . WE_BASE_URL . "\";" . PHP_EOL;
+                $jsContent .= "}". PHP_EOL;
+                //Quebra de conteúdo
+                $jsContent .= PHP_EOL;
+                //Função BaseUrl()- END
+
+                //Função ThemeBaseUrl()- START
+                $jsContent .= "function ThemeBaseUrl()". PHP_EOL;
+                $jsContent .= "{". PHP_EOL;
+                $jsContent .= "\t";
+                $jsContent .= "return BaseUrl() + \"layout/themes/" . ((WE_THEME != '') ? WE_THEME . "/" : '') . "\";" . PHP_EOL;
+                $jsContent .= "}". PHP_EOL;
+                //Quebra de conteúdo
+                $jsContent .= PHP_EOL;
+                //Função ThemeBaseUrl()- END
+
+                //Iniciando escrita
+                file_put_contents($jsFile, $jsContent);
+            }
+        }
+    }
+}
+die('hire');
---
+++ layout/themes/assets/js/scripts/weframework/weframewok.js
@@ -0,0 +1,16 @@
+/*
+# WeFramework helper base functions
+# File update at 2014-12-03 21:30:40
+*/
+
+
+function BaseUrl()
+{
+	return "http://localhost/dpg-framework-2/";
+}
+
+function ThemeBaseUrl()
+{
+	return BaseUrl() + "layout/themes/";
+}
+
