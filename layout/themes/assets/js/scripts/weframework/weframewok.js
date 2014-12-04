9a5143abc7c246a799a2f9c7b0172caea194ccad
Diogo Brito <brito@grupodpg.com.br>
2014-12-03 22:04:52 -0200

    Fix Base URL Theme

---
+++ layout/themes/assets/js/scripts/weframework/weframewok.js
@@ -0,0 +1,16 @@
+/*
+# WeFramework helper base functions
+# File update at 2014-12-03 21:44:27
+*/
+
+
+function BaseUrl()
+{
+	return "http://localhost/dpg-framework-2/";
+}
+
+function ThemeBaseUrl()
+{
+	return BaseUrl() + "layout/themes/";
+}
+
